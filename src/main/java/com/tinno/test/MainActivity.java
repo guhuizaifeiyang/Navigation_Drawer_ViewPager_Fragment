f85667eea2d189ce0dba675950c0eab557887c8e
weijie <weijie.wang@tinno.com>
2016-02-03 09:04:17 +0800

    implement navigation drawer with fragment

--- src/main/java/com/tinno/test/MainActivity.java
+++ src/main/java/com/tinno/test/MainActivity.java
@@ -3,12 +3,19 @@ package com.tinno.test;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.tinno.test.com.tinno.test.fragment.BlankFragment;
+import com.tinno.test.com.tinno.test.fragment.Fragment1;
+import com.tinno.test.com.tinno.test.fragment.Fragment2;
+import com.tinno.test.com.tinno.test.fragment.Fragment3;
 
 public class MainActivity extends AppCompatActivity {
     private DrawerLayout mDrawer;
@@ -37,6 +44,9 @@ public class MainActivity extends AppCompatActivity {
 
         // Tie DrawerLayout events to the ActionBarToggle
         mDrawer.setDrawerListener(mDrawerToggle);
+
+        // Set up drawer item
+        setupDrawerContent(navigationView);
 
     }
 
@@ -81,4 +91,53 @@ public class MainActivity extends AppCompatActivity {
         navigationView.getMenu().findItem(R.id.nav_about).setIcon(R.drawable.information);
 
     }
+
+    private void setupDrawerContent(NavigationView navigationView) {
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        selectDrawerItem(menuItem);
+                        return true;
+                    }
+                });
+    }
+
+    public void selectDrawerItem(MenuItem menuItem) {
+        // Create a new fragment and specify the planet to show based on
+        // position
+        Fragment fragment = null;
+
+        Class fragmentClass;
+        switch(menuItem.getItemId()) {
+            case R.id.nav_library:
+                fragmentClass = Fragment1.class;
+                break;
+            case R.id.nav_playlists:
+                fragmentClass = Fragment2.class;
+                break;
+            case R.id.nav_queue:
+                fragmentClass = Fragment3.class;
+                break;
+            default:
+                fragmentClass = BlankFragment.class;
+                break;
+        }
+
+        try {
+            fragment = (Fragment) fragmentClass.newInstance();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // Insert the fragment by replacing any existing fragment
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.flContent, fragment).commit();
+
+        // Highlight the selected item, update the title, and close the drawer
+        menuItem.setChecked(true);
+        setTitle(menuItem.getTitle());
+        mDrawer.closeDrawers();
+    }
+
 }
